0e1a1a2fbf59fc3404deb362d7e23996e16710eb
Sevann Radhak <sevann.radhak@inworx.com>
2020-03-23 16:22:13 -0300

    SeedDB prev

---
+++ Soccer.Web/Data/SeedDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Soccer.Web.Data
+{
+    public class SeedDB
+    {
+        private readonly DataContext _contex;
+
+        public SeedDB(DataContext contex)
+        {
+            _contex = contex;
+        }
+    }
+}
